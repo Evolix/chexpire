84fe348f17d532b9a88189beff1ddaf0c9f979d2
Jérémy Lecour <jeremy.lecour@gmail.com>
2018-06-13 22:11:04 +0200

    Improve code style, based on Rubocop recommendations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_devise_parameters, if: :devise_controller?
   before_action :set_locale
+
   protected
 
   def configure_devise_parameters
