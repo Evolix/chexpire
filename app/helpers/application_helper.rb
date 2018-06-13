9b155e018cad7d817c0e887320ed2714566edd3d
Jérémy Lecour <jeremy.lecour@gmail.com>
2018-06-13 20:28:37 +0200

    Real label for locale selection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def format_utc(time, format: :default)
     l(time.utc, format: format)
   end
+
+  def available_locales_collection
+    I18n.available_locales.map { |k| [t("shared.locales.#{k}"), k]}
+  end
 end
