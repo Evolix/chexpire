49786cda63b8e6f605f6337b9e87bbb3909b75b1
Gregory Colpart <gcolpart+git@evolix.fr>
2018-08-05 22:23:10 +0200

    use @example.com email address by default

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'chexpire@evolix.org'
+  config.mailer_sender = 'chexpire@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
