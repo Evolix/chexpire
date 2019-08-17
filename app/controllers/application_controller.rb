b32d931a2b9a2a646ab59112143f800d97a1fa88
Jérémy Lecour <jlecour@evolix.fr>
2019-08-17 12:24:13 +0200

    Use absolute path for "render file"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,6 +45,6 @@ class ApplicationController < ActionController::Base
   end
 
   def render_404
-    render file: "#{Rails.root}/public/404", status: :not_found
+    render file: "#{Rails.root}/public/404.html", status: :not_found
   end
 end
