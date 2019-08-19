a9f23bd3f52db83ca0dc306f7f1397392f709dc8
Jeremy Lecour <jlecour@evolix.fr>
2019-08-19 13:49:34 +0200

    Make Capistrano dependency a bit looser

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.2"
+lock "~> 3.10"
 
 set :application, "chexpire"
 set :repo_url, "https://github.com/Evolix/chexpire.git"
