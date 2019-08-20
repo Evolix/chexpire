a7010d76891224fa7f2a90f83fbb578f05b0c0bb
Jérémy Lecour <jlecour@evolix.fr>
2019-08-20 08:26:44 +0200

    Migrate webdriver configuration

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,7 +4,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :headless_chrome
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   def teardown
     Warden.test_reset!
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -39,17 +39,6 @@ class ActiveSupport::TestCase
   include ChexpireAssertions
 end
 
-# Capybara configuration
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    "chromeOptions" => { args: %w[headless disable-gpu] + ["window-size=1280,800"] },
-  )
-  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
-end
-Capybara.save_path = Rails.root.join("tmp/capybara")
-Capybara.javascript_driver = :headless_chrome
-Capybara.default_driver = :headless_chrome
-
 # Disable Open4 real system calls
 # require "open4"
 # require "errors"
