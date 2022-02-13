6086b0a48f7eeaf91bc12420b5cda9080413d751
Jeremy Lecour <jlecour@evolix.fr>
2022-02-13 20:25:16 +0100

    Disable ecdsa-sha2 keys for SSH

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,6 @@ append :linked_dirs,
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 set :migration_role, :app
+
+Net::SSH::Transport::Algorithms::ALGORITHMS.values.each { |algs| algs.reject! { |a| a =~ /^ecd(sa|h)-sha2/ } }
+Net::SSH::KnownHosts::SUPPORTED_TYPE.reject! { |t| t =~ /^ecd(sa|h)-sha2/ }
